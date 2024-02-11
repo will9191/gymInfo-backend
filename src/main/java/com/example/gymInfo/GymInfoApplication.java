ee61059bd1389bc35470009ff47fa08c4213afb4
will9191 <willian.fernando040@gmail.com>
2024-02-11 03:22:39 -0300

    feat: initial config

---
+++ src/main/java/com/example/gymInfo/GymInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymInfoApplication.class, args);
+	}
+
+}
